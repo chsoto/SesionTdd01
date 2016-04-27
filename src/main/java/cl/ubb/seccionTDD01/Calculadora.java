6969d7d1fa46335429436dc07621bfd64e47ac2c
chsoto <chsoto@gmail.com>
2016-04-27 12:20:02 -0300

    secto commit

--- src/main/java/cl/ubb/seccionTDD01/Calculadora.java
+++ src/main/java/cl/ubb/seccionTDD01/Calculadora.java
@@ -5,8 +5,8 @@ public class Calculadora {
 	public int sumar(int numero1, int numero2) {
 		return(numero1+numero2);
 	}
-	public int restar(int i, int j){
-		return 127;
+	public int restar(int numero1, int numero2){
+		return (numero1-numero2);
 	}
 
 }
