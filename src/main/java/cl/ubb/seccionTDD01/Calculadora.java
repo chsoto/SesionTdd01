26e4eec72d3708fd369f1db369c372ca37b3fea9
chsoto <chsoto@gmail.com>
2016-04-27 12:30:45 -0300

    test n8 exitoso

--- src/main/java/cl/ubb/seccionTDD01/Calculadora.java
+++ src/main/java/cl/ubb/seccionTDD01/Calculadora.java
@@ -8,8 +8,8 @@ public class Calculadora {
 	public int restar(int numero1, int numero2){
 		return (numero1-numero2);
 	}
-	public int division(int i, int j){
-		return 4;
+	public int division(int dividendo, int divisor){
+		return (dividendo/divisor);
 	}
 
 }
